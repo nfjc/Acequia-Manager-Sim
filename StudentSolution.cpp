77e178f2fc1efac0168db7fdb655dffdf936a865
Noah Casey <ncasey@triton.unm.edu>
2025-05-13 15:09:38 -0600

    V.1

--- StudentSolution.cpp
+++ StudentSolution.cpp
@@ -17,6 +17,7 @@
     Given the current state of the region, students will be asked to utilize the canals that connect regions to
     develop the logic and algorithm for finding a solution. The simulation has a fixed time.
 
+<<<<<<< HEAD
     The student solution will be evaluated on the criteria that each region meets the following:
     - a given region is NOT flooded
     - a given region is NOT in drought
@@ -98,3 +99,46 @@ void solveProblems(AcequiaManager& manager)
     }
 }
 
+=======
+	The student solution will be evaluated on the criteria that each region meets the following:
+	- a given region is NOT flooded
+	- a given region is NOT in drought
+	- the region waterNeed does not exceed the region waterLevel 
+*/
+
+/*This will be how the solveProblems function is set up. The student may enter their on  */
+void solveProblems(AcequiaManager& manager)
+{
+	//the student can call the members of the canals object such as name of canal. sourceRegion, and destinationRegion
+	//This could be helpful in informing the students strategy to solve the problem
+	auto canals = manager.getCanals();
+	auto regions = manager.getRegions();
+
+	while(!manager.isSolved && manager.hour!=manager.SimulationMax)
+	{ 
+		//enter student code here
+		if(manager.hour == 0) {
+			for (auto canal : canals) {
+				canal->toggleOpen(true);
+				canal->setFlowRate(0.5);
+			}
+		}
+
+		// adjust based on region status
+//Our code to solve!
+		for (auto region : regions) {
+			for (auto canal : canals) {
+				if (canal->destinationRegion == region) {
+					if (region->isInDrought) {
+						canal->setFlowRate(1.0); // full blast if in drought
+					} else if (region->isFlooded) {
+						canal->setFlowRate(0.1); // slow drip if flooded
+					}
+				}
+			}
+		}
+
+		manager.nexthour();
+	}
+}
+>>>>>>> 3aba904... Our final submission
